2ecf1632251a691ac0a84d268d9c04a33bdacc17
hafrankVT <hfrank3@radford.edu>
2016-10-11 10:31:58 -0400

    XSS Fix

--- src/main/java/org/owasp/webgoat/session/DatabaseUtilities.java
+++ src/main/java/org/owasp/webgoat/session/DatabaseUtilities.java
@@ -2,6 +2,7 @@
 package org.owasp.webgoat.session;
 
 import java.io.IOException;
+import java.security.MessageDigest;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -96,6 +97,20 @@ public class DatabaseUtilities
 
 		String userPrefix = context.getDatabaseUser();
 		String password = context.getDatabasePassword();
+		try{
+			MessageDigest digest = MessageDigest.getInstance("SHA-256");
+			byte[] hash = digest.digest(password.getBytes("UTF-8"));
+			StringBuffer hexString = new StringBuffer("");
+			
+			for (int i = 0; i < hash.length; i++){
+				String hex = Integer.toHexString(0xff & hash[i]);
+				if(hex.length() == 1) hexString.append('0');
+				hexString.append(hex);
+			}
+			password = hexString.toString();
+		}catch (Exception ex){
+			throw new RuntimeException(ex);
+		}
 		String url = context.getDatabaseConnectionString();
 		return DriverManager.getConnection(url, userPrefix + "_" + user, password);
 		} catch (ClassNotFoundException cnfe)
